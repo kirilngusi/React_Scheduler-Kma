33cf440e238e7cbd347a2168bfd2b062ed41a783
Aka.Kiril <vuongtuan1211@gmail.com>
2022-04-29 18:33:13 +0700

    fix apiurl

--- src/utilities/request.js
+++ src/utilities/request.js
@@ -1,26 +1,26 @@
-export const apiUrl = "https://api-crawl-tkb.vercel.app/";
+// export const apiUrl = "https://api-crawl-tkb.vercel.app/";
 
-export const getData = async (url, token) => {
-    const res = await fetch(`${apiUrl}/${url}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-    });
+// export const getData = async (url, token) => {
+//     const res = await fetch(`${apiUrl}/${url}`, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
 
-    return await res.json();
-};
+//     return await res.json();
+// };
 
-export const postData = async (url, data, token) => {
-    const res = await fetch(`${apiUrl}/${url}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(data),
-    });
+// export const postData = async (url, data, token) => {
+//     const res = await fetch(`${apiUrl}/${url}`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//         },
+//         body: JSON.stringify(data),
+//     });
 
-    return await res.json();
-};
+//     return await res.json();
+// };
--- src/utilities/typeContains.js
+++ src/utilities/typeContains.js
@@ -1,2 +1,4 @@
-export const apiUrl =
-    process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "";
+// export const apiUrl =
+//     process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "";
+
+export const apiUrl = "https://api-crawl-tkb.vercel.app";
